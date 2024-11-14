002557f75aff4a9c2148724180edcb82f39390c4
Distortions81 <carlotto81@gmail.com>
2024-11-14 10:01:47 -0700

    screw it, simplify it

--- logo.lua
+++ logo.lua
@@ -35,82 +35,14 @@ function LOGO_DrawLogo(force)
                 storage.SM_Store.spawnLogo.destroy()
             end
 
-            --Check if any buildings are on top of spawn
-            local blocked = false
-            local cpos = UTIL_GetDefaultSpawn()
-            local entFound = msurf.find_entities({ { x = cpos.x - 10, y = cpos.y - 10 }, { x = cpos.x + 10, y = cpos.y + 10 } })
-            for _, ent in pairs(entFound) do
-                if string.find(ent.name, "tree") then
-                    ent.destroy()
-                elseif ent.name ~= "character" and ent.has_flag("player-creation") then
-                    blocked = true
-                end
+            local cpos = msurf.find_non_colliding_position("character", { x = 0, y = 0 }, 4096, 4, true)
+            if not cpos then
+                cpos = { x = 0, y = 0 }
             end
 
-            --If needed, move spawn
-            if blocked then
-                local lpos = { x = 0, y = 0 }
-                cpos = lpos
-                local attempts = 0
-                local stillBlocked = false
-                while blocked do
-                    for x = 0, 4000, 4 do
-                        for y = 0, 4000, 4 do
-                            for z = 0, 3, 1 do
-                                if z == 0 then
-                                    lpos.x = cpos.x + x
-                                    lpos.y = cpos.y + y
-                                elseif z == 1 then
-                                    lpos.x = cpos.x - x
-                                    lpos.y = cpos.y + y
-                                elseif z == 2 then
-                                    lpos.x = cpos.x + x
-                                    lpos.y = cpos.y - y
-                                else
-                                    lpos.x = cpos.x - x
-                                    lpos.y = cpos.y - y
-                                end
-                                local entFound = msurf.find_entities({ { lpos.x - 10, lpos.y - 10 }, { lpos.x + 10, lpos.y + 10 } })
-                                attempts = attempts + 1
-                                if attempts >= 10000 then
-                                    return
-                                end
-                                stillBlocked = false
-                                for _, ent in pairs(entFound) do
-                                    if string.find(ent.name, "tree") then
-                                        ent.destroy()
-                                    elseif ent.name ~= "character" and ent.has_flag("player-creation") then
-                                        stillBlocked = true
-                                    end
-                                end
-                                if true then --msurf.can_place_entity("crash-site-spaceship", lpos) then
-                                    if not stillBlocked then
-                                        blocked = false
-                                        goto done
-                                    else
-                                        stillBlocked = false
-                                        blocked = true
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-
-
-                ::done::
-                if not blocked then
-                    local pforce = game.forces["player"]
-                    if pforce then
-                        pforce.set_spawn_position(lpos, msurf)
-                        UTIL_MapPin()
-                        UTIL_MsgAllSys("Items placed on top of the spawn area, spawn moved to " .. UTIL_GPSPos(lpos))
-                    else
-                        UTIL_ConsolePrint("[ERROR] dodrawlogo: Player force not found.")
-                    end
-                else
-                    UTIL_ConsolePrint("[ERROR] dodrawlogo: No suitable spawn location found!!!")
-                end
+            local pforce = game.forces["player"]
+            if pforce then
+                pforce.set_spawn_position(cpos, msurf)
             end
 
             -- Set drawn flag
